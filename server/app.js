9facb2a18e2d3e98a23218eeebf8392ce03b8b83
dinhthangbkhn <dinhthangbkhn@gmail.com>
2017-09-14 11:18:06 +0700

    comment everything not necessary

--- server/app.js
+++ server/app.js
@@ -12,16 +12,16 @@ var robot_direction = require('./routes/robot_direction');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/robot', robot_direction);
 // app.use('/', index);
