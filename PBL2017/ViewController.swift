55d77541831a7b3dba9ed3016095ef78ba953464
Thanh Doi <thanh.doikhac@gmail.com>
2017-09-20 17:24:33 +0700

    Update voice recognize

--- PBL2017/ViewController.swift
+++ PBL2017/ViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import Speech
 
 class ViewController: UIViewController {
     
     let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
     
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var detectedTextLabel: UILabel!
+    
+    let audioEngine = AVAudioEngine()
+    let speechRecognizer = SFSpeechRecognizer.init(locale: Locale(identifier: "en-US"))
+    let request = SFSpeechAudioBufferRecognitionRequest()
+    var recognitionTask: SFSpeechRecognitionTask?
+    var isRecording = false
+    
     func sendCommand(_ direction: String) {
-        let server = "http://192.168.11.26:3000/robot/\(direction)"
+        let server = "http://192.168.11.38:3300/robot/\(direction)"
         print(server)
         let url = URL(string: server)
         let dataTask = defaultSession.dataTask(with: url!) {
@@ -32,22 +43,34 @@ class ViewController: UIViewController {
         }
         dataTask.resume()
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        self.requestSpeechAuthorization()
     }
-
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.landscape
+    }
+    
+    override var preferredInterfaceOrientationForPresentation: UIInterfaceOrientation {
+        return UIInterfaceOrientation.landscapeRight
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     @IBAction func straightButtonPressed(_ sender: Any) {
         sendCommand("straight")
     }
-
+    
     @IBAction func turnLeftButtonPressed(_ sender: Any) {
         sendCommand("left")
     }
@@ -58,6 +81,104 @@ class ViewController: UIViewController {
     
     @IBAction func rotateButtonPressed(_ sender: Any) {
         sendCommand("rotate")
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        if isRecording == true {
+            cancelRecording()
+            isRecording = false
+            startButton.setImage(UIImage(named: "stop"), for: .normal)
+        } else {
+            self.recordAndRecognizeSpeech()
+            isRecording = true
+            startButton.setImage(UIImage(named: "start"), for: .normal)
+        }
+    }
+    
+    func cancelRecording() {
+        audioEngine.stop()
+        if let node = audioEngine.inputNode {
+            node.removeTap(onBus: 0)
+        }
+        recognitionTask?.cancel()
+    }
+    
+    func recordAndRecognizeSpeech() {
+        guard let node = audioEngine.inputNode else { return }
+        let recordingFormat = node.outputFormat(forBus: 0)
+        node.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
+            self.request.append(buffer)
+        }
+        audioEngine.prepare()
+        do {
+            try audioEngine.start()
+        } catch {
+            self.sendAlert(message: "There has been an audio engine error.")
+            return print(error)
+        }
+        guard let myRecognizer = SFSpeechRecognizer() else {
+            self.sendAlert(message: "Speech recognition is not supported for your current locale.")
+            return
+        }
+        if !myRecognizer.isAvailable {
+            self.sendAlert(message: "Speech recognition is not currently available. Check back at a later time.")
+            // Recognizer is not available right now
+            return
+        }
+        recognitionTask = speechRecognizer?.recognitionTask(with: request, resultHandler: { result, error in
+            if let result = result {
+                
+                let bestString = result.bestTranscription.formattedString
+                
+                var lastString: String = ""
+                for segment in result.bestTranscription.segments {
+                    self.detectedTextLabel.text = lastString
+                    let indexTo = bestString.index(bestString.startIndex, offsetBy: segment.substringRange.location)
+                    lastString = bestString.substring(from: indexTo)
+                }
+                self.checkForDirectionSaid(resultString: lastString)
+            } else if let error = error {
+                print(error)
+            }
+        })
+    }
+    
+    func requestSpeechAuthorization() {
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    self.startButton.isEnabled = true
+                case .denied:
+                    self.startButton.isEnabled = false
+                case .restricted:
+                    self.startButton.isEnabled = false
+                case .notDetermined:
+                    self.startButton.isEnabled = false
+                }
+            }
+        }
+    }
+    
+    func checkForDirectionSaid(resultString: String) {
+        let resultString = resultString.lowercased()
+        switch resultString {
+        case "left":
+            sendCommand("left")
+        case "right":
+            sendCommand("right")
+        case "straight":
+            sendCommand("straight")
+        case "rotate":
+            sendCommand("rotate")
+        default: break
+        }
+    }
+    
+    func sendAlert(message: String) {
+        let alert = UIAlertController(title: "Speech Recognizer Error", message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
